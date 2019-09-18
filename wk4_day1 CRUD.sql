85ddc8d6d953ac533cad3507dd68e1e319433fe6
jaywiles <jay.wiles@gmail.com>
2019-09-17 20:02:46 -0500

    Jay Wiles - done

---
+++ wk4_day1 CRUD.sql
@@ -0,0 +1,45 @@
+-- need to fix something
+-- DELETE FROM
+-- 	users
+-- WHERE
+-- 	id = 510;
+    
+
+#3 below
+-- DELETE FROM
+-- 	users
+-- WHERE
+-- 	id = 114;
+
+-- DELETE FROM
+-- 	usersAddress
+-- WHERE
+-- 	user_id = 114;
+
+-- DELETE FROM
+-- 	usersContact
+-- WHERE
+-- 	user_id = 114;
+
+-- #2 below
+
+-- UPDATE
+-- 	usersAddress
+-- SET
+-- 	state = 'REDACTED'
+-- WHERE
+-- 	state = 'OH'
+
+-- #1 below
+-- DESC users;
+
+-- INSERT INTO users
+-- 	(first_name, last_name)
+--     VALUES
+-- 	('test', 'user'),
+--     ('test2', 'user');
+--     
+-- SELECT
+-- 	id
+-- FROM
+-- 	users
